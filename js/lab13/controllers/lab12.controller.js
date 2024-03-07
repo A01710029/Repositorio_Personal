e671a1ca1aa8fadee0b450a77699912ca23345f5
A01710029 <palmadamartinez@gmail.com>
2024-03-07 11:11:57 -0600

    Agregar controllers y models Lab 13

---
+++ js/lab13/controllers/lab12.controller.js
@@ -0,0 +1,4 @@
+exports.getLab12 = (request, response, next) => {
+    response.render("lab12.ejs");
+    next();
+}
---
+++ js/lab13/controllers/lab13.controller.js
@@ -0,0 +1,4 @@
+exports.getLab13 = (request, response, next) => {
+    response.render("lab13.ejs");
+    next();
+}
